c7d80b1f3a175347de541d4377c10a2b05ee81ce
AppMaker123 <cory.ginsberg1@gmail.com>
2015-02-18 21:27:15 -0600

    Test

---
+++ Pods/Target Support Files/Pods-Homework Helper-JVFloatLabeledTextField/Pods-Homework Helper-JVFloatLabeledTextField-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Homework_Helper_JVFloatLabeledTextField : NSObject
+@end
+@implementation PodsDummy_Pods_Homework_Helper_JVFloatLabeledTextField
+@end
