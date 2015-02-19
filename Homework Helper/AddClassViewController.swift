f7b0a5e2f3c22052267a2d8b03596b0695bda0f5
AppMaker123 <cory.ginsberg1@gmail.com>
2015-02-19 01:23:04 -0600

    Started redoing the entire Add Class page in code.

--- Homework Helper/AddClassViewController.swift
+++ Homework Helper/AddClassViewController.swift
@@ -9,101 +9,72 @@
 import UIKit
 import CoreData
 
-class AddClassViewController: UITableViewController {
-    
-    @IBOutlet weak var txtStartDate: UILabel!
-    @IBOutlet weak var startDatePicker: UIDatePicker!
-    @IBOutlet weak var txtEndDate: UILabel!
-    @IBOutlet weak var endDatePicker: UIDatePicker!
-    @IBOutlet var daysOfWeekControl: UISegmentedControl!
-    
-    private var startDatePickerHidden = false
-    private var endDatePickerHidden = false
+class AddClassViewController: UIViewController {
 
+    let fieldHeight:CGFloat = 50.0
+    let fieldMargin:CGFloat = 10.0
+    let fieldFontSize:CGFloat = 18.0
+    let fieldFloatingLabelFontSize:CGFloat = 15.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
         navigationController?.navigationBar.shadowImage = UIImage(named: "green-shadow")
-        startDatePicker.date = NSDate()
-        endDatePicker.date = NSDate()
-        startDidChangeDate()
-        endDidChangeDate()
-        toggleStartDatePicker()
-        toggleEndDatePicker()
         
-    }
-    
-    @IBAction internal func startDidChangeDate() {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat =  "yyyy-MM-dd hh:mm:ss"
-        let compareResult = startDatePicker.date.compare(endDatePicker.date)
-        if compareResult == NSComparisonResult.OrderedDescending {
-            startDatePicker.setDate(endDatePicker.date, animated: true)
-        }
-        txtStartDate.text = NSDateFormatter.localizedStringFromDate(startDatePicker.date, dateStyle: .ShortStyle, timeStyle: .ShortStyle)
+        title = NSLocalizedString("Add Class", comment: "Title for the page. Page allows user to add a new class.")
+        
+        let floatingLabelColor = view.tintColor
+        
+        let div0 = UIView()
+        div0.frame = CGRect(x: fieldMargin, y: 4, width: view.frame.size.width - (2.0 * fieldMargin), height: 1.0)
+        div0.backgroundColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.0)
+        view.addSubview(div0)
+        
+        //Defines the Class Name Field
+        let classNameField = JVFloatLabeledTextField(frame: CGRect(x: fieldMargin , y: div0.frame.origin.y + div0.frame.size.height, width: view.frame.size.width - (2 * fieldMargin), height: fieldHeight))
+        classNameField.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("Class Name", comment: "The Name of the Class"), attributes: [NSForegroundColorAttributeName: UIColor.darkGrayColor()])
+        classNameField.font = UIFont.systemFontOfSize(fieldFontSize)
+        classNameField.floatingLabel.font = UIFont.boldSystemFontOfSize(fieldFloatingLabelFontSize)
+        classNameField.clearButtonMode = .WhileEditing
+        view.addSubview(classNameField)
+        
+        let div1 = UIView()
+        div1.frame = CGRect(x: fieldMargin, y: classNameField.frame.origin.y + classNameField.frame.size.height, width: view.frame.size.width - (2 * fieldMargin), height: 1.0)
+        div1.backgroundColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.3)
+        view.addSubview(div1)
+        
+        //Defines the Select Teacher Button
+        let teacherButton = UIButton(frame: CGRect(x: fieldMargin , y: div1.frame.origin.y + div1.frame.size.height, width: view.frame.size.width - (2 * fieldMargin), height: fieldHeight))
+        teacherButton.buttonType
+        teacherButton.setAttributedTitle(NSAttributedString(string: NSLocalizedString("Select Teacher", comment: "The Name of the Class"), attributes: [NSForegroundColorAttributeName: UIColor.darkGrayColor()]), forState: .Normal)
+        teacherButton.titleLabel?.font = UIFont.systemFontOfSize(fieldFontSize)
+        teacherButton.contentHorizontalAlignment = .Left;
+        let size = teacherButton.frame.size
+        let arrow = UIImage(named: "drop-down-arrow")!
+        teacherButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: teacherButton.frame.width - 35, bottom: 0, right: 0)
+        teacherButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: -arrow.size.width, bottom: 0, right: 0)
 
-    }
-    
-    @IBAction internal func endDidChangeDate() {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat =  "yyyy-MM-dd hh:mm:ss"
-        let compareResult = endDatePicker.date.compare(startDatePicker.date)
-        if compareResult == NSComparisonResult.OrderedAscending {
-            endDatePicker.setDate(startDatePicker.date, animated: true)
-        }
-        txtEndDate.text = NSDateFormatter.localizedStringFromDate(endDatePicker.date, dateStyle: .ShortStyle, timeStyle: .ShortStyle)
-    }
-    
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        switch (indexPath.section, indexPath.row) {
-        case (1, 0):
-            toggleStartDatePicker()
-            if !endDatePickerHidden {
-                toggleEndDatePicker()
-            }
-        case (1, 2):
-            toggleEndDatePicker()
-            if !startDatePickerHidden {
-                toggleStartDatePicker()
-            }
-        default:
-            if !startDatePickerHidden {
-                toggleStartDatePicker()
-            }
-            if !endDatePickerHidden {
-                toggleEndDatePicker()
-            }
-            break
-        }
-    }
-    
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-
-        if startDatePickerHidden && indexPath.section == 1 && indexPath.row == 1 {
-            return 0
-        } else if endDatePickerHidden && indexPath.section == 1 && indexPath.row == 3 {
-            return 0
-        } else {
-            return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
-        }
-    }
-    
-    private func toggleStartDatePicker() {
-        startDatePickerHidden = !startDatePickerHidden
+        teacherButton.setImage(arrow, forState: .Normal)
+        teacherButton.selected = false
+        view.addSubview(teacherButton)
         
-        // Force table to update its contents
-        tableView.beginUpdates()
-        tableView.endUpdates()
-    }
-    
-    private func toggleEndDatePicker() {
-        endDatePickerHidden = !endDatePickerHidden
+        let div2 = UIView()
+        div2.frame = CGRect(x: fieldMargin, y: teacherButton.frame.origin.y + teacherButton.frame.size.height, width: view.frame.size.width - (2 * fieldMargin), height: 1.0)
+        div2.backgroundColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.3)
+        view.addSubview(div2)
         
-        // Force table to update its contents
-        tableView.beginUpdates()
-        tableView.endUpdates()
+        let blankDiv = UIView()
+        blankDiv.frame = CGRect(x: fieldMargin, y: div2.frame.origin.y + div2.frame.size.height, width: view.frame.size.width - (2 * fieldMargin), height: fieldHeight)
+        blankDiv.backgroundColor = UIColor.clearColor().colorWithAlphaComponent(0.0) // Extra Clear!
+        view.addSubview(blankDiv)
+        
+        let div3 = UIView()
+        div3.frame = CGRect(x: fieldMargin, y: blankDiv.frame.origin.y + blankDiv.frame.size.height, width: view.frame.size.width - (2 * fieldMargin), height: 1.0)
+        div3.backgroundColor = UIColor.lightGrayColor().colorWithAlphaComponent(0.3)
+        view.addSubview(div3)
+        // TODO: Add Two Buttons for the start and end date of the class with an arrow on the right side. The dropdown will be a calendar (NOT a date picker).
     }
     
     override func didReceiveMemoryWarning() {
